347d6ecd7f18e8091251a108fd90c27d0a59aba5
yejin <yj040102@naver.com>
2023-12-03 16:21:19 +0900

    eslint

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import '../index.css';
-import styled from 'styled-components'
 import LoginControl from "./LoginControl";
 
 export default function Header() {
